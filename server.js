f1850f0924fc8b8c839285c0afe610ca174c41e8
Nguyen Manh Du 20165913 <du.nm165913@sis.hust.edu.vn>
2019-04-27 01:04:48 +0700

    feat: Auto delte 1day(not pay) or 30 day(pay MoMo)

--- server.js
+++ server.js
@@ -1,29 +1,51 @@
-const Express=require('express');
+const Express = require('express');
 const app = Express();
-const models=require('./models');
-const bodyParser= require('body-parser')
-const path= require('path')
+const models = require('./models');
+const bodyParser = require('body-parser')
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op
+const path = require('path')
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(Express.static(path.join(__dirname, 'public')))
 
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 app.use(Express.static(__dirname + '/public'));
 
+setInterval(() => {
+    models.sanpham.findAll({
+        where: {
+
+            [Op.or]: [
+                {
+                    trangthai: "Yes",
+                    createdAt: {
+                        [Op.lt]: new Date(new Date() - 24 * 60 * 60 * 1000 * 30)
+                    }
+                }, {
+                    trangthai: "No",
+                }
+            ]
+        }
+    }).then(re => {
+        re.map(x => x.destroy())
+    })
+}, 24 * 60 * 60 * 1000);
 
 
-models.sequelize.sync().then(()=>{
+models.sequelize.sync().then(() => {
     console.log("fine")
-}).catch((err)=>{
+}).catch((err) => {
     console.log(err)
 })
 
 require('./routes')(app);
-   
-    
 
-app.listen(1000,()=>{
+
+
+app.listen(1000, () => {
     console.log("server is starting at 1000 ")
 })
 
-module.exports=app
+module.exports = app
